601500d4061b2b4dca860a3dfc78c6c898007915
BabithaReddy7 <babithareddy077@gmail.com>
2025-02-24 19:34:50 +0530

    Updated SQLite path for Render

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Initialize SQLite Database
-const db = new sqlite3.Database('./todo.db', (err) => {
+const db = new sqlite3.Database('/tmp/todo.db', (err) => {
     if (err) throw err;
     console.log("✅ Connected to SQLite database");
 });
